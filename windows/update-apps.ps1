4791a16451f10143cacbc9ea393873980d252aea
Tony Ward <tonygward@googlemail.com>
2025-10-18 11:05:46 +0100

    Improved error logging

--- windows/update-apps.ps1
+++ windows/update-apps.ps1
@@ -1,10 +1,20 @@
 
-$dir = 'C:\Scheduled\logs'; if (!(Test-Path $dir)) { New-Item -ItemType Directory -Path $dir | Out-Null }
+# Fail fast on errors
+$ErrorActionPreference = 'Stop'
+
+# Use a logs directory relative to this script's location
+$dir = Join-Path $PSScriptRoot 'logs'
+if (!(Test-Path $dir)) {
+    New-Item -ItemType Directory -Path $dir | Out-Null
+}
 
 $log = Join-Path $dir ('winget-' + (Get-Date -Format yyyyMMdd-HHmmss) + '.log')
-winget upgrade --all --silent --accept-package-agreements --accept-source-agreements --disable-interactivity *>&1 |
+winget source update --disable-interactivity *>&1 |
+    Tee-Object -FilePath $log -Append
+winget upgrade --all --silent --accept-package-agreements --disable-interactivity *>&1 |
     Tee-Object -FilePath $log -Append
 
 $log = Join-Path $dir ('choco-' + (Get-Date -Format yyyyMMdd-HHmmss) + '.log')
 choco feature enable -n=allowGlobalConfirmation
+choco outdated *>&1 | Tee-Object -FilePath $log -Append
 choco upgrade all -y --no-progress *>&1 | Tee-Object -FilePath $log -Append
